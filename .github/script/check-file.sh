feee1624a20e675c489260921d684ff04b6b88a4
Mikolajczyk <przemek.mikolajczyk@intel.com>
2022-09-05 13:18:20 +0200

    addd check file

---
+++ .github/script/check-file.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+# Make sure this file is executable
+# chmod a+x .github/script/check-file.sh
+
+# Make sure to escape your backslashes => \\ <= in YAML
+# So that its still a single \ in bash
+
+echo "Shell of ubuntu"
+
+echo uname -r
