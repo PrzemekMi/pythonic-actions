3f9c55f790e419bfaa122ada037cf229e61b09a6
Mikolajczyk <przemek.mikolajczyk@intel.com>
2022-09-05 11:41:48 +0200

    add pandas

--- platform_info.py
+++ platform_info.py
@@ -1,7 +1,9 @@
 """ example python file which doesn't do anything specific """
 
 import platform
+import pandas as pd
 
 
 if __name__ == '__main__':
     print(f"CI platform info {platform.platform()}")
+    print(f"Pandas version {pd.__version__}")
